fb3625ef53f6d3001673d8bee8930f120fd1dc59
ykzzyk <nz9024295@gmail.com>
2022-11-03 10:38:32 -0500

    version 0.0.1 compeleted

--- visualization/app.py
+++ visualization/app.py
@@ -1,7 +1,9 @@
 import pathlib
 import logging
+import numpy as np
 
 import pyvista as pv
+from scipy.spatial.transform import Rotation as R
 
 logger = logging.getLogger("visualization")
 
@@ -11,9 +13,15 @@ class App:
         
         # Show output
         self.pl = pv.Plotter()
-        self.pl.add_axes(interactive=True)
         self.actors = {}
         self.actor_attrs = {}
+        
+        # "xy" view
+        self.xyviewcamera = pv.Camera()
+        self.xyviewcamera.position = (7.75202772140503, 3.917879838943482, 53.657579687507386)
+        self.xyviewcamera.focal_point = (7.75202772140503, 3.917879838943482, 0.4370880126953125)
+        self.xyviewcamera.up = (0.0, 1.0, 0.0)
+        
         
     def load_image(self, image_path:pathlib.Path, scaling:list=[1,1,1]):
         
@@ -37,6 +45,15 @@ class App:
         self.actors["image-origin"] = actor.copy()
         self.actor_attrs['image'] = actor_attr
         
+    def degree2matrix(self, r: list, t: list):
+        rot = R.from_euler("xyz", r, degrees=True)
+        rot = rot.as_matrix()
+        
+        trans = np.array(t).reshape((-1, 1))
+        matrix = np.vstack((np.hstack((rot, trans)), np.array([0, 0, 0, 1])))
+        
+        return matrix
+        
     def load_mesh(self, mesh_path:pathlib.Path, name:str):
         
         # Check if the file exists
@@ -46,40 +63,55 @@ class App:
         
         # Create image mesh and add it to the plotter
         mesh = pv.read(mesh_path)
+        
+        # r = [8.770189620706784, 171.674855383245, 49.08692066857242]
+        # t = [11.83933198174675, 5.512584484779442, -4.1060903869260095]
+        
+        # transform_matrix = self.convert2transform_matrix(r, t)
+        
+        # mesh.rotate_x(8.770189620706784, inplace=True)
+        # mesh.rotate_y(171.674855383245, inplace=True)
+        # mesh.rotate_z(49.08692066857242, inplace=True)
+        
+        # mesh = mesh.transform(transform_matrix)
+        
         actor = self.pl.add_mesh(mesh, rgb=True)
+        
+        # actor.orientation = (21.84230194065212, 44.86586576118945, 171.99906466676183)
+        # actor.position = (4.192207212217855, 7.394309210439285, -0.8389455453076233)
+        
+        actor.orientation = (13.93752422036519, 49.441080905025686, 170.56188741343297)
+        actor.position = (4.4715937628911275, 7.283363364481435, -0.7639411050175622)
+        
         actor, actor_attr = self.pl.add_actor(actor, name=name)
         
         # Save actor for later
         self.actors[name] = actor
         self.actor_attrs[name] = actor_attr
         
-    def report_registration(self, *args):
+    def reset_camera_event(self, *args):
+        self.pl.camera = self.xyviewcamera.copy()
+        logger.debug("reset_camera_event callback complete")
+            
+    def reset_image_position(self, *args):
+        self.actors["image"] = self.actors["image-origin"].copy() # have to use deepcopy to prevent change self.actors["image-origin"] content
+        self.pl.add_actor(self.actors["image"], name="image")
+        logger.debug("reset_image_position callback complete")
+        
+    def track_registration(self, *args):
         
         for actor_name, actor in self.actors.items():
-            logger.debug(f"<Actor {actor_name}> T: {actor.position} R: {actor.orientation}")
+            logger.debug(f"<Actor {actor_name}> R: {actor.orientation}, T: {actor.position}")
         
     def plot(self):
         
-        # "xy" view
-        # pl.camera_position = [(7.75202772140503, 3.917879838943482, 53.657579687507386), (7.75202772140503, 3.917879838943482, 0.4370880126953125), (0, 1, 0)]
-
+        self.pl.add_axes()
         self.pl.enable_joystick_actor_style()
     
-        def reset_camera_event():
-            self.pl.camera.position = (7.75202772140503, 3.917879838943482, 53.657579687507386)
-            self.pl.camera.focal_point = (7.75202772140503, 3.917879838943482, 0.4370880126953125)
-            self.pl.camera.up = (0.0, 1.0, 0.0)
-            logger.debug("reset_camera_event callback complete")
-            
-        def reset_image_position():
-            self.actors["image"] = self.actors["image-origin"].copy()
-            self.pl.add_actor(self.actors["image"], name="image")
-            logger.debug("reset_image_position callback complete")
-        
         # Register callbacks
-        self.pl.add_key_event('c', reset_camera_event)
-        self.pl.add_key_event('i', reset_image_position)
-        self.pl.track_click_position(self.report_registration, side='left')
+        self.pl.add_key_event('c', self.reset_camera_event)
+        self.pl.add_key_event('i', self.reset_image_position)
+        self.pl.add_key_event('t', self.track_registration)
         
         # add the camera orientation to move the camera
         _ = self.pl.add_camera_orientation_widget()
@@ -87,3 +119,6 @@ class App:
         # Actual presenting
         cpos = self.pl.show(cpos="xy", return_cpos=True)
         logger.debug(f"cpos: {cpos}")
+        logger.debug(f"ossicles_orientation: {self.actors['ossicles'].orientation}")
+        logger.debug(f"ossicles_orientation: {self.actors['ossicles'].position}")
+    
