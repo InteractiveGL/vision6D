452a267b27d8baf681662a94055899fda5bac7a5
ykzzyk <yike.zhang@vanderbilt.edu>
2023-02-15 11:46:37 -0600

    changes

--- test/test_create_dataset.py
+++ test/test_create_dataset.py
@@ -22,7 +22,7 @@ import PIL
 logger = logging.getLogger("vision6D")
 np.set_printoptions(suppress=True)
 
-# size of the (1920, 1080)
+# size: (1920, 1080)
 @pytest.fixture
 def app():
     return vis.App(register=True, scale=1)
@@ -151,19 +151,19 @@ def test_generate_image(app):
 
 @pytest.mark.parametrize(
     "name, hand_draw_mask, ossicles_path, RT, resize, mirror_objects",
-    [("455", None, vis.config.OSSICLES_MESH_PATH_455_right, vis.config.gt_pose_455_right, 1/5, False), # no resize: 0.05338873922462614 # resize 1/5 cv2: 0.026132524126728313 # if resize torch: 
+    [("455", None, vis.config.OSSICLES_MESH_PATH_455_right, vis.config.gt_pose_455_right, 1/10, False), # no resize: 0.05338873922462614 # resize 1/5 cv2: 0.026132524126728313 # if resize torch: 
         
-    ("5997", None, vis.config.OSSICLES_MESH_PATH_5997_right, vis.config.gt_pose_5997_right, 1/5, False), # no resize: 0.021086712065792698 # resize 1/5 cv2: 0.020330257484100347 # if resize torch: 
-    ("5997",  vis.config.mask_5997_hand_draw_numpy, vis.config.OSSICLES_MESH_PATH_5997_right, vis.config.gt_pose_5997_right, 1/5, False), # error: 1.3682088051366954 # if resize cv2: 1.0566890956912622 # if resize torch: 
+    ("5997", None, vis.config.OSSICLES_MESH_PATH_5997_right, vis.config.gt_pose_5997_right, 1/10, False), # no resize: 0.021086712065792698 # resize 1/5 cv2: 0.020330257484100347 # if resize torch: 
+    ("5997",  vis.config.mask_5997_hand_draw_numpy, vis.config.OSSICLES_MESH_PATH_5997_right, vis.config.gt_pose_5997_right, 1/10, False), # error: 1.3682088051366954 # if resize cv2: 1.0566890956912622 # if resize torch: 
     
-    ("6088", None, vis.config.OSSICLES_MESH_PATH_6088_right, vis.config.gt_pose_6088_right, 1/5, False), # no resize: 5.491416579722634 # resize 1/5 cv2: 5.534377619304417 # if resize torch: 
-    ("6088", vis.config.mask_6088_hand_draw_numpy, vis.config.OSSICLES_MESH_PATH_6088_right, vis.config.gt_pose_6088_right, 1/5, False), # error: 5.636555974411995 # if resize cv2: 5.46914034648768 # if resize torch: 
+    ("6088", None, vis.config.OSSICLES_MESH_PATH_6088_right, vis.config.gt_pose_6088_right, 1/10, False), # no resize: 5.491416579722634 # resize 1/5 cv2: 5.534377619304417 # if resize torch: 
+    ("6088", vis.config.mask_6088_hand_draw_numpy, vis.config.OSSICLES_MESH_PATH_6088_right, vis.config.gt_pose_6088_right, 1/10, False), # error: 5.636555974411995 # if resize cv2: 5.46914034648768 # if resize torch: 
     
-    ("6108", None, vis.config.OSSICLES_MESH_PATH_6108_right, vis.config.gt_pose_6108_right, 1/5, False), # no resize: 0.01895783336598894 # resize 1/5 cv2: 0.1413067780393649 # if resize torch: 
-    ("6108", vis.config.mask_6108_hand_draw_numpy, vis.config.OSSICLES_MESH_PATH_6108_right, vis.config.gt_pose_6108_right, 1/5, False), # error: 0.3925129080313312 # if resize cv2: 22.451005863827966 # if resize torch: 
+    ("6108", None, vis.config.OSSICLES_MESH_PATH_6108_right, vis.config.gt_pose_6108_right, 1/10, False), # no resize: 0.01895783336598894 # resize 1/5 cv2: 0.1413067780393649 # if resize torch: 
+    ("6108", vis.config.mask_6108_hand_draw_numpy, vis.config.OSSICLES_MESH_PATH_6108_right, vis.config.gt_pose_6108_right, 1/10, False), # error: 0.3925129080313312 # if resize cv2: 22.451005863827966 # if resize torch: 
     
-    ("6742", None, vis.config.OSSICLES_MESH_PATH_6742_left, vis.config.gt_pose_6742_left, 1/5, False), # no resize: 0.04155607588744 # resize 1/5 cv2: 0.13983189316096442 # if resize torch: 
-    ("6742", vis.config.mask_6742_hand_draw_numpy, vis.config.OSSICLES_MESH_PATH_6742_left, vis.config.gt_pose_6742_left, 1/5, False), # error: 2.2111475894404378 # if resize cv2: 146.63621797086526 # if resize torch: 
+    ("6742", None, vis.config.OSSICLES_MESH_PATH_6742_left, vis.config.gt_pose_6742_left, 1/10, False), # no resize: 0.04155607588744 # resize 1/5 cv2: 0.13983189316096442 # if resize torch: 
+    ("6742", vis.config.mask_6742_hand_draw_numpy, vis.config.OSSICLES_MESH_PATH_6742_left, vis.config.gt_pose_6742_left, 1/10, False), # error: 2.2111475894404378 # if resize cv2: 146.63621797086526 # if resize torch: 
     
     # ("6742", mask_6742_hand_draw_numpy, OSSICLES_MESH_PATH_6742_left, gt_pose_6742_right, 1/5, True), # no resize: 5.214560773437986 # resize 1/5 cv2: 230.26984657453482 # if resize torch: 
     ]
@@ -180,13 +180,15 @@ def test_pnp_from_dataset(name, hand_draw_mask, ossicles_path, RT, resize, mirro
     np.save(vis.config.DATA_DIR / "gt_pose" / gt_pose_name, RT)
     app = vis.App(register=True, scale=1, mirror_objects=mirror_objects)
 
+    w, h = app.window_size
+
     if hand_draw_mask is not None: 
         seg_mask = np.load(hand_draw_mask).astype("bool")
         plt.imshow(seg_mask)
         plt.show()
     # no segmentation mask, use the whole mask to predict
     else: 
-        seg_mask = np.ones((1080, 1920)).astype("bool")
+        seg_mask = np.ones((h, w)).astype("bool")
 
     if mirror_objects: seg_mask = seg_mask[..., ::-1]
 
@@ -210,15 +212,21 @@ def test_pnp_from_dataset(name, hand_draw_mask, ossicles_path, RT, resize, mirro
     # save the rendered partial image
     if hand_draw_mask is not None: vis.utils.save_image(color_mask, vis.config.DATA_DIR / "rendered_mask", f"rendered_mask_partial_{name}.png")
     
-    resize_width = int(1920 * resize)
-    resize_height = int(1080 * resize)
-    downscale_color_mask = cv2.resize(color_mask, (resize_width, resize_height), interpolation = cv2.INTER_AREA)
-    downscale_binary_mask = cv2.resize(binary_mask, (resize_width, resize_height), interpolation = cv2.INTER_AREA)
     # numpy implementation
-    color_mask = cv2.resize(downscale_color_mask, (1920, 1080), interpolation = cv2.INTER_AREA)
-    binary_mask = cv2.resize(downscale_binary_mask, (1920, 1080), interpolation = cv2.INTER_AREA)
+    resize_height = int(h * resize)
+    resize_width = int(w * resize)
+    downscale_color_mask = cv2.resize(color_mask, (resize_width, resize_height), interpolation=cv2.INTER_AREA)
+    downscale_binary_mask = cv2.resize(binary_mask, (resize_width, resize_height), interpolation=cv2.INTER_AREA)
+        
+    # based on the ratio of the mask size compare with the whole image
+    if np.sum(downscale_binary_mask) / (downscale_binary_mask.shape[0] * downscale_binary_mask.shape[1]) > 0.01:
+        color_mask = cv2.resize(downscale_color_mask, (w, h), interpolation=cv2.INTER_LINEAR)
+        binary_mask = cv2.resize(downscale_binary_mask, (w, h), interpolation=cv2.INTER_LINEAR)
+    else:
+        color_mask = cv2.resize(downscale_color_mask, (w, h), interpolation=cv2.INTER_AREA)
+        binary_mask = cv2.resize(downscale_binary_mask, (w, h), interpolation=cv2.INTER_AREA)
     # # torch implementation
-    # trans = torchvision.transforms.Resize((1080, 1920))
+    # trans = torchvision.transforms.Resize((h, w))
     # color_mask = trans(torch.tensor(downscale_color_mask).permute(2,0,1))
     # color_mask = color_mask.permute(1,2,0).detach().cpu().numpy()
     # binary_mask = trans(torch.tensor(downscale_binary_mask).unsqueeze(-1).permute(2,0,1))
@@ -248,4 +256,4 @@ def test_pnp_from_dataset(name, hand_draw_mask, ossicles_path, RT, resize, mirro
             
     logger.debug(f"\ndifference from predicted pose and RT pose: {np.sum(np.abs(predicted_pose - RT))}")
             
-    assert np.isclose(predicted_pose, RT, atol=50).all()
+    assert np.isclose(predicted_pose, RT, atol=20).all()
