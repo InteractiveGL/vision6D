58a3bfd99177916bce1d572e08931eebede17bca
ykzzyk <yike.zhang@vanderbilt.edu>
2023-05-15 16:14:03 -0500

    package

--- vision6D/run_gui.py
+++ vision6D/run_gui.py
@@ -2,10 +2,14 @@ from vision6D import Interface_GUI
 import sys
 from qtpy import QtWidgets
 import vision6D as vis
+import pathlib
+import os
+
+GITROOT = pathlib.Path(os.path.abspath(__file__)).parent.parent
 
 def exe():
     app = QtWidgets.QApplication(sys.argv)
-    stylesheet_pth = vis.config.GITROOT / "vision6D" / "data" / "style.qss"
+    stylesheet_pth = GITROOT / "vision6D" / "style.qss"
     with open(stylesheet_pth, "r") as file: stylesheet = file.read()  # Replace with the path to your .qss file
     app.setStyleSheet(stylesheet)
     window = Interface_GUI()
