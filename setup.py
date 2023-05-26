441a18148705d30f71ee45b52cb6d44dc63c54e6
ykzzyk <yike.zhang@vanderbilt.edu>
2023-05-26 09:36:43 -0500

    updates

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 # For development mode
 import setuptools
 
-setuptools.setup()
+setuptools.setup(
+    name = "vision6D",
+    packages=setuptools.find_packages(),
+    include_package_data=True
+)
