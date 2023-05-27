7a9c0c71ff6bfd1c0c74770b65fa76f4abd182e5
ykzzyk <yike.zhang@vanderbilt.edu>
2023-05-26 19:14:35 -0500

    changes

--- setup.py
+++ setup.py
@@ -5,12 +5,4 @@ setuptools.setup(
     name = "vision6D",
     packages=setuptools.find_packages(),
     include_package_data=True,
-    extras_require={
-        ':sys_platform == "win32"': [
-            'opencv-python',
-        ],
-        ':sys_platform == "linux"': [
-            'opencv-python-headless',
-        ],
-    },
 )
