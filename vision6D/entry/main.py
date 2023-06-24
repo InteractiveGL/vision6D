9f1b5ab334995fc3b096ddc8bd5d1b3fa39e2d69
ykzzyk <yike.zhang@vanderbilt.edu>
2023-06-23 23:15:46 -0500

    add entry points

--- vision6D/entry/main.py
+++ vision6D/entry/main.py
@@ -2,16 +2,17 @@ import sys
 import os
 import pathlib
 from qtpy import QtWidgets
-from vision6D import Interface
+
+from .. import Interface
 
 CWD = pathlib.Path(os.path.abspath(__file__)).parent
 
-def exe():
+def main():
     app = QtWidgets.QApplication(sys.argv)
-    with open(CWD / "data" / "style.qss", "r") as f: app.setStyleSheet(f.read())
+    with open(CWD.parent / "data" / "style.qss", "r") as f: app.setStyleSheet(f.read())
     window = Interface()
     window.showMaximized()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
-    exe()
+    main()
